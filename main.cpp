f6fd9f4bd0a7afcd670ce2137001da6797adb63d
b3nt0s <bentonthat11@gmail.com>
2024-06-06 13:00:34 -0700

    low taper

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main() {
 
     cout << "Hello World" << endl;
-    cout << "sussy among us" << endl;
+    cout << "ryan is gay" << endl;
+    cout << "Who thinks ryan should get a low taper fade?" << endl;
+    bool agree = true;
+    cout << agree;
 
     return 0;
 }
