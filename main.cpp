7a5e3edaaf177c91a1142afeb71aa109b5da510b
RyanDang363 <ryandang999@gmail.com>
2024-06-18 22:28:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,5 +17,13 @@ void displayIntro() {
     cout << "The purpose of this application is to organize patients for easy access" << endl 
          << "to prevent confusion and inefficiency in crucial times. In other times " << endl
          << "just for the convenience and quick to find patients without much effort."<< endl;
-
 }
+void displayMenu() {
+    cout << "[1] - add new patient manually" << endl;
+    cout << "[2] - add new patient(s) from input file" << endl;
+    cout << "[3] - delete patient" << endl;
+    cout << "[4] - undo delete patient" << endl;
+    cout << "[5] - add new patient" << endl;
+    cout << "[6] - exit application" << endl;
+}
+
